ed5632c7cd1a45547cdfdad1c5840b84596c72c8
Keezill <mamedov.murik.lezgin@gmail.com>
2021-06-15 18:44:04 +0300

    Initial release of hw13

--- src/main/java/com/alevel/Main.java
+++ src/main/java/com/alevel/Main.java
@@ -4,23 +4,40 @@ import com.alevel.model.DriverLicence;
 import com.alevel.model.Person;
 
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.Optional;
 
 public class Main {
 
     public static void main(String[] args) {
         final DriverLicence driverLicence = DriverLicence.builder()
-                                                         .categories(new String[]{"A", "B", "C"})
-                                                         .expireDate(Instant.parse("2020-11-30T18:35:24.00Z"))
-                                                         .build();
+                .categories(new String[]{"A", "B", "C"})
+                .expireDate(Instant.parse("2021-11-30T18:35:24.00Z"))
+                .build();
         final Person person = Person.builder()
-                                    .firstName("John")
-                                    .lastName("Smith")
-                                    .driverLicence(Optional.ofNullable(driverLicence))
-                                    .build();
+                .firstName("John")
+                .lastName("Smith")
+                .driverLicence(Optional.ofNullable(driverLicence))
+                .build();
 
+        Instant currentDate = Instant.now();
         Optional.ofNullable(person)
-                // you code here
-        ;
+                .flatMap(Person::getDriverLicence)
+                .filter(driverLicence1 -> currentDate.isBefore(driverLicence1.getExpireDate()))
+                .map(DriverLicence::getCategories)
+                .ifPresentOrElse(categories -> System.out.println(Arrays.toString(categories)),
+                        () -> {
+                            throw new RuntimeException("Driver license is not valid");
+                        });
+
+
+
+        /*Optional<DriverLicence> licence = Optional.ofNullable(person)
+                .map(Person::getDriverLicence) // getting driver licence from the person
+                .filter(drvLicence -> drvLicence.isPresent() && drvLicence.get().getExpireDate().isAfter(currentDate)) // checking if driver licence is present and not expired
+                .orElseThrow(() -> new RuntimeException("Driver licence not valid (not present or expired")); // throw exception if expired or not present
+        // Printing categories
+        licence.ifPresent(driverLicence1 -> Arrays.stream(driverLicence1.getCategories()).forEach(System.out::println));
+         */
     }
 }
